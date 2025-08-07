9008107203309e3a5004d873dea02b30abc7c85e
Eliseu Brito <eliseubrito@protonmail.com>
2025-08-07 19:20:11 -0300

    Adicionado menu de texto com opções

---
+++ src/MenuOpcoes.java
@@ -0,0 +1,15 @@
+public class MenuOpcoes {
+    public static void exibir() {
+        System.out.println("***********************************");
+        System.out.println("Conversor de Moeda $");
+        System.out.println();
+        System.out.println("1) Dólar Americano para Real");
+        System.out.println("2) Euro para Real");
+        System.out.println("3) Libra para Real");
+        System.out.println("4) Real para Dólar Americano");
+        System.out.println("5) Real para Euro");
+        System.out.println("6) Real para Libra");
+        System.out.println("7) Sair");
+        System.out.print("Escolha uma opção: ");
+    }
+}
